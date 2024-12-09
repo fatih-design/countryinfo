9b6cac98732fa39dcd1950eb6e3944b16f6b106a
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 12:25:29 +0100

    haal landen op

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App(){
-    return <h1>Landenoverzicht</h1>;
+import axios from 'axios'
+import error from "eslint-plugin-react/lib/util/error.js";
+
+
+function App() {
+    const fetchCountries = async () => {
+        try {
+            const response = await axios.get("https://restcountries.com/v3.1/all");
+            console.log(response.data)
+        } catch (error) {
+            console.error('Er is iets misgegaan', error);
+        }
+    };
+    return (
+        <>
+        <h1>Landen overzicht</h1>
+            <button onClick = {fetchCountries}>Haal landen op</button>
+</>
+    );
 }
 
 export default App;
