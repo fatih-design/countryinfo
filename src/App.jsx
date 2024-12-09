0bf020a719300da3cb99d5a0d8c684d04f727c70
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 16:57:45 +0100

    kleur toegevoegd aan regio

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
             const response = await axios.get("https://restcountries.com/v3.1/all");
             const sortedCountries = response.data
                 .map((country) => ({
-                 name: country?.name.common || "Onbekend",
+                    name: country?.name.common || "Onbekend",
                     population: country?.population || 0,
                     flags: country?.flags.png || "",
+                    region: country?.region || "Onbekend",
 
                 }))
                 .sort((a, b) => a.population - b.population);
+
             setCountries(sortedCountries);
         } catch (error) {
             console.error('Er is iets misgegaan', error);
@@ -25,7 +27,14 @@ function App() {
             <h1>Landen overzicht</h1>
             <button onClick={fetchCountries}>Haal landen op</button>
             <ul>{countries.map((country, index) => (
-                <li key={index}>{country.name} - {country.population} - <img
+                <li key={index}
+                style={{
+                    color:
+                    country.region === "Africa" ? "blue" :
+                    country.region === "Europe" ? "yellow" :
+                    country.region === "Americas" ? "green" :
+                    country.region === "Asia" ? "red" :
+                    country.region === "Oceania" ? "purple" : "black"}}>{country.name} - {`Has a population of ${country.population}`} - <img
                     src={country.flags} alt={`Vlag van ${country.name}`}
                     width="20"/></li>
             ))}
