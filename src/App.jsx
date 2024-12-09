290dd48073fe47423fadff8e2dec665a60c27ebb
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 20:39:32 +0100

    zoekbalk

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
-import error from "eslint-plugin-react/lib/util/error.js";
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
+
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [search,setSearch] = useState('');
+    const [filteredCountries, setFilteredCountries] = useState([]);
     const fetchCountries = async () => {
         try {
             const response = await axios.get("https://restcountries.com/v3.1/all");
@@ -18,23 +20,49 @@ function App() {
                 .sort((a, b) => a.population - b.population);
 
             setCountries(sortedCountries);
+
+            setfilteredCountries(sortedCountries);
         } catch (error) {
             console.error('Er is iets misgegaan', error);
         }
+
     };
+    useEffect(() => {
+        const filtred = countries.filter((country) =>
+            country.name.toLowerCase().includes(search.toLowerCase())
+
+
+        );setFilteredCountries(filtred);
+    }, [search,countries]);
     return (
-        <>
+        <><h2>Zoeken</h2>
+            <input type="text" onChange={(e) => setSearch(e.target.value)} value={search}/>
+
+            <ul>
+                {filteredCountries.map((country, index) => (
+                    <li key={index}>
+                        {country.name} - {`Has a population of ${country.population}`} -
+                        <img
+                            src={country.flags}
+                            alt={`Vlag van ${country.name}`}
+                            width="20"
+                        />
+                    </li>
+                ))}
+            </ul>
+
             <h1>Landen overzicht</h1>
             <button onClick={fetchCountries}>Haal landen op</button>
             <ul>{countries.map((country, index) => (
                 <li key={index}
-                style={{
-                    color:
-                    country.region === "Africa" ? "blue" :
-                    country.region === "Europe" ? "yellow" :
-                    country.region === "Americas" ? "green" :
-                    country.region === "Asia" ? "red" :
-                    country.region === "Oceania" ? "purple" : "black"}}>{country.name} - {`Has a population of ${country.population}`} - <img
+                    style={{
+                        color:
+                            country.region === "Africa" ? "blue" :
+                                country.region === "Europe" ? "yellow" :
+                                    country.region === "Americas" ? "green" :
+                                        country.region === "Asia" ? "red" :
+                                            country.region === "Oceania" ? "purple" : "black"
+                    }}>{country.name} - {`Has a population of ${country.population}`} - <img
                     src={country.flags} alt={`Vlag van ${country.name}`}
                     width="20"/></li>
             ))}
