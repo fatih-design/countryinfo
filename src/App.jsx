a83fc9ee94d9cddcec093ecedb6c12f206ff14a4
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 15:27:34 +0100

    gesorteerde lijst populatie laag-hoog

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import axios from 'axios'
 import error from "eslint-plugin-react/lib/util/error.js";
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
 function App() {
     const [countries, setCountries] = useState([]);
     const fetchCountries = async () => {
         try {
             const response = await axios.get("https://restcountries.com/v3.1/all");
-            setCountries(response.data);
+            const sortedCountries = response.data
+                .sort((a, b) => a.population - b.population);
+            setCountries(sortedCountries);
         } catch (error) {
             console.error('Er is iets misgegaan', error);
         }
     };
     return (
         <>
-        <h1>Landen overzicht</h1>
-            <button onClick = {fetchCountries}>Haal landen op</button>
-            <ul>{countries.map((country,index) =>(
-                <li key={index}>{country?.name.common || "onbekend"} - {country?.population || "onbekend"} - <img src={country?.flags.png || "onbekend"} alt={`Vlag van ${country.name.common || "onbekend"}`} width = "20" /></li>
-                ))}
+            <h1>Landen overzicht</h1>
+            <button onClick={fetchCountries}>Haal landen op</button>
+            <ul>{countries.map((country, index) => (
+                <li key={index}>{country?.name.common || "onbekend"} - {country?.population || "onbekend"} - <img
+                    src={country?.flags.png || "onbekend"} alt={`Vlag van ${country.name.common || "onbekend"}`}
+                    width="20"/></li>
+            ))}
             </ul>
-</>
+        </>
     );
 }
 
