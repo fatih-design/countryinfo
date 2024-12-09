6104199cabaf09175072b0b51b15a21cb009628b
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 10:54:03 +0100

    landenoverzicht titel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import worldmap from "./assets/world_map.png";
 
 function App() {
 
     return (
         <>
+            <span><img src = {worldmap}/></span>
             Maak je applicatie hier!
         </>
     )
