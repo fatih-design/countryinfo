3adcd08eb9857dbd60d8fd447689a4e891f5f263
fatih-design <tukenmezfatih@gmail.com>
2024-12-09 13:03:14 +0100

    state en map toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import error from "eslint-plugin-react/lib/util/error.js";
-
+import React,{useState} from 'react';
 
 function App() {
+    const [countries, setCountries] = useState([]);
     const fetchCountries = async () => {
         try {
             const response = await axios.get("https://restcountries.com/v3.1/all");
-            console.log(response.data)
+            setCountries(response.data);
         } catch (error) {
             console.error('Er is iets misgegaan', error);
         }
@@ -15,6 +16,10 @@ function App() {
         <>
         <h1>Landen overzicht</h1>
             <button onClick = {fetchCountries}>Haal landen op</button>
+            <ul>{countries.map((country,index) =>(
+                <li key={index}>{country.name.common}</li>
+                ))}
+            </ul>
 </>
     );
 }
